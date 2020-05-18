3631d7b0e23f4d9616b24bd1c70be2d01507c824
blakexdd <blakexxxd@gmail.com>
2020-05-18 17:49:27 +0300

    initial commit

---
+++ short_links/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+	res.render('index');
+})
+
+app.post('/', function(req, res){
+	res.render('index');
+	console.log(req.body.link);
+})
+
+app.listen(3000, function(){
+	console.log('Example app listening on port 3000!')
+})
+
